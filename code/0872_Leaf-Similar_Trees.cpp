34184c88c35d22bbc263efd93cad7f9a7eba6c7c
Honfung Wong <21543236+onshek@users.noreply.github.com>
2020-06-13 23:32:16 +0800

    update 0872

--- code/0872_Leaf-Similar_Trees.cpp
+++ code/0872_Leaf-Similar_Trees.cpp
@@ -14,8 +14,41 @@ public:
         vector<int> tnV1, tnV2;
         inOrder(root1, tnV1);
         inOrder(root2, tnV2);
-        if (tnV1 == tnV2) return true;
-        return false;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        // inOrder traverse
+        vector<int> tnV1, tnV2;
+        inOrder(root1, tnV1);
+        inOrder(root2, tnV2);
+        return tnV1 == tnV2;
+    }
+
+    void inOrder(TreeNode* root, vector<int>& tnV) {
+        if (!root) return;
+        stack<TreeNode*> tnStack;
+        while (true) {
+            if (root) {
+                tnStack.push(root);
+                root = root->left;
+            } else if (!tnStack.empty()) {
+                root = tnStack.top();
+                if (!root->left & !root->right) tnV.push_back(root->val);
+                tnStack.pop();
+                root = root->right;
+            } else break;
+        }
+    }
+};
     }
 
     void inOrder(TreeNode* root, vector<int>& tnV) {
