a6caf42e07853ddb9391615596c03a0c690fc49f
Honfung Wong <21543236+onshek@users.noreply.github.com>
2021-02-28 23:05:44 +0800

    UPD: 0001

--- code/0001_Two_Sum.cpp
+++ code/0001_Two_Sum.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
-        for (int i=0; i<nums.size()-1; i++) {
-            for (int j=i+1; j<nums.size(); j++) {
-                if ((target - nums[i]) == nums[j]) {
-                    result.push_back(i); 
-                    result.push_back(j);
-                    return result;
-                }
+        unordered_map<int, int> result;
+        for (int i=0; i<nums.size(); i++) {
+            auto it = result.find(target - nums[i]);
+            if (it != result.end()) {
+                return {it->second, i};
             }
+            result[nums[i]] = i;
         }
-        return result;
+    return {};
     }
 };
+
+
+/*
+执行用时：8 ms, 在所有 C++ 提交中击败了75.34% 的用户
+内存消耗：8.6 MB, 在所有 C++ 提交中击败了90.44% 的用户
+*/
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         vector<int> result;
+//         for (int i=0; i<nums.size()-1; i++) {
+//             for (int j=i+1; j<nums.size(); j++) {
+//                 if ((target - nums[i]) == nums[j]) {
+//                     result.push_back(i); 
+//                     result.push_back(j);
+//                     return result;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// };
